3ec3c786327cc2a932bb826645e8e2146c6339c1
wangcong <wangcong001@sensorsdata.cn>
2024-05-22 20:43:31 +0800

    feat: try thiserror and anyhow error

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Context;
+use ecosystem::MyError;
+use std::{fs, mem::size_of};
+
+fn main() -> Result<(), anyhow::Error> {
+    println!("size of MyError is {}", size_of::<MyError>());
+
+    let filename = "non-existent-file.txt";
+    let _fd =
+        fs::File::open(filename).with_context(|| format!("Can not find file: {}", filename))?;
+
+    fail_with_error()?;
+
+    Ok(())
+}
+
+fn fail_with_error() -> Result<(), MyError> {
+    Err(MyError::Custom("This is a custom error".to_string()))
 }
