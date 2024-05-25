1c71be11eabfa4df1884140efea3e5f79f56ae77
wangcong <wangcong001@sensorsdata.cn>
2024-05-25 10:30:52 +0800

    feat: tracing

--- examples/err.rs
+++ examples/err.rs
@@ -1,6 +1,19 @@
 use anyhow::Context;
-use ecosystem::MyError;
 use std::{fs, mem::size_of};
+
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum MyError {
+    #[error("I/O error: {0}")]
+    Io(#[from] std::io::Error),
+    #[error("Parse error: {0}")]
+    Parse(#[from] std::num::ParseIntError),
+    #[error("Serialize json error: {0}")]
+    Serialize(#[from] serde_json::Error),
+    #[error("Custom error: {0}")]
+    Custom(String),
+}
 
 fn main() -> Result<(), anyhow::Error> {
     println!("size of MyError is {}", size_of::<MyError>());
